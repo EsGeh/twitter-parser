5d8622e2c53c294cafcf2df64015d078948b474b
EsGeh <SamuelGfroerer@googlemail.com>
2020-05-10 19:24:26 +0200

    write tweets to database. change database schema

--- scripts/initdb/initdb.sql
+++ scripts/initdb/initdb.sql
@@ -5,26 +5,20 @@ CREATE TABLE IF NOT EXISTS keywords (
 );
 
 CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL, 
-  age INTEGER,
-  gender TEXT,
-  nationality TEXT
+  id TEXT PRIMARY KEY,
+  name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tweets (
-  id INT PRIMARY KEY,
-  name TEXT NOT NULL, 
-  age INTEGER,
-  gender TEXT,
-  nationality TEXT,
-	user_id INT,
+  id TEXT PRIMARY KEY,
+	text TEXT,
+	user_id TEXT NOT NULL,
 	FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS keywords_tweets_rel (
-	keyword TEXT,
-  tweet_id INT,
+	keyword TEXT NOT NULL,
+  tweet_id TEXT NOT NULL,
 	FOREIGN KEY (keyword) REFERENCES keywords(keyword),
 	FOREIGN KEY (tweet_id) REFERENCES tweets(id)
 );
--- src/python/twitter_parser.py
+++ src/python/twitter_parser.py
@@ -23,17 +23,98 @@ def init_db():
 def db_get_keywords(connection):
     cursor = connection.cursor()
     cursor.execute(
-        # sql stmt:
-        """SELECT (keyword) FROM keywords"""
+        """SELECT (keyword) FROM keywords;"""
     )
     ret = cursor.fetchall()
     return list(map( lambda x: x[0], ret))
 
 def db_write_tweet(
+        db_connection,
         keyword,
         tweet_json,
 ):
-    pass
+    cursor = db_connection.cursor()
+    # test if tweet already in db:
+    cursor.execute(
+        """
+        SELECT (id)
+        FROM tweets
+        WHERE id=%(id)s;
+        """,
+        {
+            "id": tweet_json['id_str'],
+        }
+    )
+    ret = cursor.fetchall()
+    # if already in database, do nothing:
+    if( len(ret) > 0 ):
+        print( f"already in db: '{tweet_json['id']}'" )
+        return
+    else:
+        # add user:
+        cursor.execute(
+            """
+            SELECT (id)
+            FROM users
+            WHERE id=%(user_id)s;
+            """,
+            {
+                "user_id": tweet_json['user']['id_str'],
+            }
+        )
+        ret = cursor.fetchall()
+        if len(ret) == 0:
+            cursor.execute(
+                """
+                INSERT INTO users (id,name)
+                VALUES (%(user_id)s, %(user_name)s);
+                """,
+                {
+                    "user_id": tweet_json['user']['id_str'],
+                    "user_name": tweet_json['user']['name'],
+                }
+            )
+        cursor.execute(
+            """
+            INSERT INTO tweets (id,text,user_id)
+            VALUES (%(id)s, %(text)s, %(user_id)s);
+            """,
+            {
+                "id": tweet_json['id_str'],
+                "text": tweet_json['text'],
+                "user_id": tweet_json['user']['id_str'],
+            }
+        )
+        db_connection.commit()
+    # remember (keyword, tweet):
+    cursor.execute(
+        """
+        SELECT (keyword)
+        FROM keywords_tweets_rel
+        WHERE keyword=%(keyword)s AND tweet_id=%(tweet_id)s;
+        """,
+        {
+            "keyword": keyword,
+            "tweet_id": tweet_json['id_str'],
+        }
+    )
+    ret = cursor.fetchall()
+    # if already in database, do nothing:
+    if( len(ret) > 0 ):
+        print( f"already in db: '{tweet_json['id']}'" )
+        return
+    else:
+        cursor.execute(
+            """
+            INSERT INTO keywords_tweets_rel
+            VALUES (%(keyword)s, %(tweet_id)s);
+            """,
+            {
+                "keyword": keyword,
+                "tweet_id": tweet_json['id_str'],
+            }
+        )
+        db_connection.commit()
 
 #########################
 # twitter abstraction:
@@ -78,6 +159,50 @@ def twitter_fetch_tweets(
         lowest_id = None
     return (lowest_id, json)
 
+def fetch_and_write_tweets(
+        keyword,
+        db_connection,
+        twitter_connection
+):
+    latest_tweet_id = None
+    lowest_id = None
+    loaded_tweet_count = 0
+    just_added_tweet_count = 0
+    first_pass = True
+    while( True ):
+        (lowest_id, tweets) = twitter_fetch_tweets(
+                auth,
+                [keyword],
+                max_id=lowest_id,
+        )
+        just_added_tweet_count = len(tweets['statuses'])
+        loaded_tweet_count += just_added_tweet_count
+        if first_pass and just_added_tweet_count > 0:
+            latest_tweet_id = tweets['statuses'][-1]['id']
+        # debug print:
+        print( f"received {just_added_tweet_count} tweets containing '{keyword}':" )
+        for tweet in tweets['statuses']:
+            print( f"id: {tweet['id']}, date: {tweet['created_at']}" )
+        # write to database:
+        for tweet in tweets['statuses']:
+            db_write_tweet(
+                    db_connection,
+                    keyword,
+                    tweet
+            )
+        # break loop, if no more tweets:
+        if just_added_tweet_count == 0:
+            break
+        else:
+            first_pass = False
+    return {
+            "latest_tweet_id" : latest_tweet_id,
+            "added_tweet_count" : loaded_tweet_count,
+    }
+
+    # keys for some tweet:
+    # ['created_at', 'id', 'id_str', 'text', 'truncated', 'entities', 'extended_entities', 'metadata', 'source', 'in_reply_to_status_id', 'in_reply_to_status_id_str', 'in_reply_to_user_id', 'in_reply_to_user_id_str', 'in_reply_to_screen_name', 'user', 'geo', 'coordinates', 'place', 'contributors', 'retweeted_status', 'is_quote_status', 'retweet_count', 'favorite_count', 'favorited', 'retweeted', 'possibly_sensitive', 'lang'
+
 
 #########################
 # actual program:
@@ -85,32 +210,22 @@ def twitter_fetch_tweets(
 
 if __name__ == '__main__':
     print( "connecting to database..." )
-    with init_db() as connection:
-        keywords = db_get_keywords( connection )
+    with init_db() as db_connection:
+        keywords = db_get_keywords( db_connection )
         auth = init_twitter()
         print( f"fetching tweets containing {', '.join(keywords)}..." )
 
+        keyword_to_latest_tweet_id = {}
+        for keyword in keywords:
+            keyword_to_latest_tweet_id[keyword] = None
         # iterate through every keyword and aggregate "all" tweets that contain it:
         for keyword in keywords:
             print( f"tweets with keyword '{keyword}'" )
-            lowest_id = None
-            loaded_tweet_count = 0
-            just_added_tweet_count = 0
-            while( True ):
-                (lowest_id, tweets) = twitter_fetch_tweets(
-                        auth,
-                        [keyword],
-                        max_id=lowest_id,
-                )
-                just_added_tweet_count = len(tweets['statuses'])
-                loaded_tweet_count += just_added_tweet_count
-                print( f"received {just_added_tweet_count} tweets:" )
-                for tweet in tweets['statuses']:
-                    print( f"id: {tweet['id']}, date: {tweet['created_at']}" )
-                if just_added_tweet_count == 0:
-                    break
-            print( f"for keyword '{keyword}' {loaded_tweet_count} tweets have been added." )
-
-        # keys for some tweet:
-        # ['created_at', 'id', 'id_str', 'text', 'truncated', 'entities', 'extended_entities', 'metadata', 'source', 'in_reply_to_status_id', 'in_reply_to_status_id_str', 'in_reply_to_user_id', 'in_reply_to_user_id_str', 'in_reply_to_screen_name', 'user', 'geo', 'coordinates', 'place', 'contributors', 'retweeted_status', 'is_quote_status', 'retweet_count', 'favorite_count', 'favorited', 'retweeted', 'possibly_sensitive', 'lang'
+            tweets_info = fetch_and_write_tweets(
+                    keyword,
+                    db_connection=db_connection,
+                    twitter_connection=auth
+            )
+            keyword_to_latest_tweet_id = tweets_info['latest_tweet_id']
+            print( f"for keyword '{keyword}' {tweets_info['added_tweet_count']} tweets have been added." )
         print( "closing connection to database..." )
